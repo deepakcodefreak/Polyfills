de1be4c3124cd326aac2982a0355b6fb5b995d82
DEEPAK CHAWLA <33095673+deepakcodefreak@users.noreply.github.com>
2022-07-12 23:15:15 +0530

    Update (Date.now).js

--- (Date.now).js
+++ (Date.now).js
@@ -3,3 +3,12 @@ if(!Date.now){
     return (new Date(0)).getTime()
   }
 }
+
+// other way
+// '+' operator when used with single operand, it does the toNumber coersion  
+
+if(!Date.now){
+  Date.now = function(){
+    return +new Date()
+  }
+}
